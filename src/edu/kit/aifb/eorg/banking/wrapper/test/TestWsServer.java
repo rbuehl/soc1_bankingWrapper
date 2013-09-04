a521ce89a9ad12e9a2d5a4c3dcb8dcbceef92673
Robin Fischer <robin.fischer@kit.edu>
2013-09-04 18:56:29 +0200

    This jar works

--- src/edu/kit/aifb/eorg/banking/wrapper/test/TestWsServer.java
+++ src/edu/kit/aifb/eorg/banking/wrapper/test/TestWsServer.java
@@ -1,6 +1,5 @@
 package edu.kit.aifb.eorg.banking.wrapper.test;
 
-import javax.swing.JOptionPane;
 import javax.xml.ws.Endpoint;
 
 import edu.kit.aifb.eorg.banking.wrapper.ws.BankingServiceImpl;
@@ -9,6 +8,7 @@ public class TestWsServer {
 	
 	public static void main(String[] args) {
 		String url = (args.length >0 ) ? args[0] : "http://localhost:4434/bankingservice";
+		System.out.println("Starting with url: " + url);
 		Endpoint ep = Endpoint.publish(url, new BankingServiceImpl());
 	}
 }
